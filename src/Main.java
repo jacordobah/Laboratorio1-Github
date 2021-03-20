5bd2d06885bc922cc7559eab444ab02b42d149ff
nathalia <iuribel@unal.edu.co>
2021-03-20 00:25:22 -0500

    Hello , commit Branch Dividir

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("hello , i'm nath");
+        int m =44;
+        int g= 11;
+        int l = m/g;
+        System.out.print(l);
+    }
+}
