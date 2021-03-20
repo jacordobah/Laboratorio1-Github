831dcd6cf8e49deb090bdf2643737ed1091e8603
jacordobah <jacordobah@unal.edu.co>
2021-03-19 20:26:22 -0500

    modulo
    funcion modulo

---
+++ src/Modulo.java
@@ -0,0 +1,15 @@
+public class Modulo{
+    int a;
+    int b;
+    
+    public Modulo(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    public int modulo (){
+        return a%b;
+    }
+
+
+
+}
